5f63179a398c1a54baee7fcfda0b51c7b638ea74
luisferher <lfhernandez11@miners.utep.edu>
2021-02-09 21:38:57 -0700

    Added comments and implemented while loop, added interface file

--- src/gui.c
+++ src/gui.c
@@ -1,10 +1,13 @@
-main(){
-char c;
-c == getchar();
-if(c == ' '){
-printf("There is a space");
-}
-else {
-printf("There is a character");
-}
+#include <stdio.h>
+
+void main(){
+  //Define character c
+  char c;
+  //Ask user for an input and store value in c variable
+  c = getchar();
+  //While loop will run until c is the end-of-file signal
+  while(c != EOF){
+    putchar(c);//Print the value of character c
+    c = getchar();//Ask user for input
+  }
 }
