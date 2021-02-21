e4ce2155faf1bd3357b3164d68a419e06246ac8e
luisferher <lfhernandez11@miners.utep.edu>
2021-02-20 17:51:56 -0700

    Changed condition on gui.c file to \n

--- src/gui.c
+++ src/gui.c
@@ -6,8 +6,8 @@ void main(){
   //Ask user for an input and store value in c variable
   c = getchar();
   //While loop will run until c is the end-of-file signal
-  while(c != EOF){
+  while(c != '\n'){
     putchar(c);//Print the value of character c
-    c = getchar();//Ask user for input
+    c = getchar();//Ask user for an input and store value in c variable
   }
 }
